8c10057bad31a73f9415a2a743ab40c3a0f2bef3
Luan Jardim <luang.ajardim@gmail.com>
2023-08-02 22:53:08 -0300

    Wip(Sending files): Sending txt and jpeg files

--- client.py
+++ client.py
@@ -1,28 +1,27 @@
 import socket as sck
 import os
 import math as m
+import time
 
 MESSAGE = b'hello ma bro'
 UDDP_IP = '127.0.0.1'
 PORT = 6969
 
+def send_file(file_name):
+    file = open(file_name, 'rb')
+
+    while(1):
+        data = file.read(1024)
+        if len(data) == 0:
+            break
+        my_socks.sendto(data, (UDDP_IP, PORT))
+
+    file.close()
+
+
 my_socks = sck.socket(sck.AF_INET,
                       sck.SOCK_DGRAM)
 
-file_info = os.stat('file.txt')
-tam = file_info.st_size / 1024
-bytes_for_id = m.ceil((m.log2(tam) / 8))
-
-file = open('file.txt', 'rb')
-file.seek(0)
-
-cnt = 0
-while(1):
-    data = file.read(1024 - bytes_for_id)
-    if len(data) == 0:
-        break
-    data = data + cnt.to_bytes(bytes_for_id, byteorder='big')
-    my_socks.sendto(data, (UDDP_IP, PORT))
-    cnt += 1
-
-file.close()
+send_file('file.txt')
+time.sleep(4)
+send_file('messiiiiiii.jpeg')
--- server.py
+++ server.py
@@ -4,13 +4,27 @@ import time
 MY_IP = '127.0.0.1'
 PORT = 6969
 
+def recv_file(socks):
+    data_total = bytes()
+    while 1:
+        my_other_socks.settimeout(3)
+        try:
+            data, addr = my_other_socks.recvfrom(1024)
+            data_total += data
+        except sck.timeout:
+            break
+    return data_total
+
 my_other_socks = sck.socket(sck.AF_INET,
                       sck.SOCK_DGRAM)
 my_other_socks.bind((MY_IP, PORT))
 
-while 1:
-    data, addr = my_other_socks.recvfrom(1024)
-    print(data)
-    #print(addr)
+data = recv_file(my_other_socks)
+fd = open('file_recv.txt', 'wb')
+fd.write(data)
+fd.close()
+data = recv_file(my_other_socks)
+fd = open('messi_retorno.jpeg', 'wb')
+fd.write(data)
+fd.close()
 
-    time.sleep(1)
